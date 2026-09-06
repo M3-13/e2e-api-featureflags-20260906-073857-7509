118078e67906a8cc340a8bf382cbd89135556f09
office-crew <development@office-crew.dev>
2026-09-06 08:10:36 +0200

    feat: Flag-Löschung über DELETE /flags/{key} implementieren
    Closes #5

--- internal/api/delete.go
+++ internal/api/delete.go
@@ -1,11 +1,21 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	"featureflags/internal/store"
 )
 
 func DeleteFlag(s *store.Store, w http.ResponseWriter, r *http.Request) {
-	WriteError(w, http.StatusNotImplemented, "not implemented")
+	key := r.PathValue("key")
+	if err := s.Delete(key); err != nil {
+		if errors.Is(err, store.ErrNotFound) {
+			WriteError(w, http.StatusNotFound, "flag not found")
+			return
+		}
+		WriteError(w, http.StatusInternalServerError, "internal error")
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
